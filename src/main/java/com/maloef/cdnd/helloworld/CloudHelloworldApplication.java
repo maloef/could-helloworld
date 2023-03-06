55a3ca897f3b3b56e7ab665385e1c84e74bf451e
Markus Loeffler <m_loeffler@gmx.de>
2023-03-06 10:52:43 +0000

    initial commit

---
+++ src/main/java/com/maloef/cdnd/helloworld/CloudHelloworldApplication.java
@@ -0,0 +1,28 @@
+package com.maloef.cdnd.helloworld;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@Slf4j
+public class CloudHelloworldApplication {
+
+	@Value("${cloud.aws.credentials.access-key}")
+	private String awsKey;
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s, key is %s", name, awsKey);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudHelloworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maloef/cdnd/helloworld/CloudHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maloef.cdnd.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
